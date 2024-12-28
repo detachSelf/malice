7f37ae8448d39bd9e3e143a07268fce86bda29fa
detachSelf <christian.gade@pm.me>
2024-12-28 16:41:08 +0100

    Added Error handling for connections

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ import pg from "pg";
 // Connect to the database using the DATABASE_URL environment
 //   variable injected by Railway
 // TESTING!
-const pool = new pg.Pool();
+const pool = new pg.Pool({
+  max: 100, // Maximum number of clients
+  idleTimeoutMillis: 300000, // Close idle clients after 5 min
+});
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -15,9 +18,15 @@ app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+  try {
+    const { rows } = await pool.query("SELECT NOW()");
+    res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+  } catch (error) {
+    console.error("Database query error:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
